1c6184713317f755c4a42f264904923cc6d732c2
MrX456 <47821251+MrX456@users.noreply.github.com>
2019-05-20 11:14:14 -0300

    Add files via upload

---
+++ JavaScript/02-Variáveis e Constantes/TiposVariaveis.js
@@ -0,0 +1,23 @@
+//Podemos exibir o tipo da variavel que declaramos atraves do comando typeof
+//Inteiros ou Reais não são diferenciados em JavaScript ambos são classificados
+//como NUMBER
+
+var x = 5;
+var y = 3.2;
+var w = "5";
+var pais = "França";
+var leitura = true;
+
+document.write("Tipos de variaveis declaradas" +"<br/>");
+document.write(x +" é " +typeof x +"<br/>");
+document.write(y +" é " +typeof y +"<br/>");
+document.write(w +" é " +typeof w +"<br/>");
+document.write(pais +" é " +typeof pais +"<br/>");
+document.write(leitura +" é " +typeof leitura +"<br/>");
+
+
+
+
+
+
+
