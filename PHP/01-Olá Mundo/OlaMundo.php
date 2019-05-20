c840d56ebceb7003eb47cc6b98326a46bc6102ad
MrX456 <47821251+MrX456@users.noreply.github.com>
2019-05-20 12:20:56 -0300

    Add files via upload

---
+++ PHP/01-Olá Mundo/OlaMundo.php
@@ -0,0 +1,13 @@
+<?php
+
+//Para escrver mensagens na tela com php usamos comando echo
+
+echo'Olá Mundo';
+
+//Quebrar linha.
+echo'<br/>';
+
+//Mensagens também podem ser escritas através do comando print
+print'Olá Mundo!';
+
+?>
---
+++ PHP/01-Olá Mundo/OlaMundoVariosIdiomas.php
@@ -0,0 +1,23 @@
+<?php
+//Tags html podem ser usadas dentro da tag php, contanto que estejam
+//dentro de um comando echo.As linhas podem ser quebradas com a tag <br/>
+
+echo'<h1> Português </h1> <br/>';
+echo'<h3> Olá Mundo! </h3> <br/>';
+
+echo'<h1> Inglês </h1> <br/>';
+echo'<h3> Hello World! </h3> <br/>';
+
+echo'<h1> Espanhol </h1> <br/>';
+echo'<h3> Hola Mundo! </h3> <br/>';
+
+echo'<h1> Italiano </h1> <br/>';
+echo'<h3> Ciao Mondo! </h3> <br/>';
+
+echo'<h1> Francês </h1> <br/>';
+echo'<h3> Bonjour le Monde! </h3> <br/>';
+
+echo'<h1> Alemão </h1> <br/>';
+echo'<h3> Hallo Welt! </h3> <br/>';
+
+?>
