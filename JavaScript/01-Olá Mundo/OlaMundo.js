55503782db1cdfdb3283b2cc82fffb232860455a
MrX456 <47821251+MrX456@users.noreply.github.com>
2019-05-20 11:12:40 -0300

    Add files via upload

---
+++ JavaScript/01-Olá Mundo/OlaMundo.js
@@ -0,0 +1,9 @@
+//Para escrever uma mensagem na tela em JavaScript temos duas opções
+
+//Desse modo a mensagem será exibida através de um popup no navegador
+alert("Ola Mundo !");
+
+//Desse modo a mensagem será diretamente escrita no documento html
+document.write("Ola Mundo !");
+
+
---
+++ JavaScript/01-Olá Mundo/OlaMundoVariosIdiomas.js
@@ -0,0 +1,20 @@
+// Ola Mundo em diversos idiomas
+alert("Ola Mundo ! Em diversos idiomas");
+
+//Para quebrar uma linha em JavaScript usamos
+//"<br/>"
+document.write("Português: Ola Mundo!" +"<br/>");
+
+document.write("Inglês: Hello World!" +"<br/>");
+
+document.write("Espanhol: Hola Mundo!" +"<br/>");
+
+document.write("Italiano: Ciao Mondo!" +"<br/>");
+
+document.write("Francês: Bonjour le Monde!" +"<br/>");
+
+document.write("Alemão: Hallo Welt!" +"<br/>");
+
+
+
+
