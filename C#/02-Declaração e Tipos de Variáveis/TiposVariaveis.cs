1a21a30c64c217de2956f1876d2e872a074d416f
MrX456 <47821251+MrX456@users.noreply.github.com>
2019-05-20 11:50:38 -0300

    Update TiposVariaveis.cs

--- C#/02-Declaração e Tipos de Variáveis/TiposVariaveis.cs
+++ C#/02-Declaração e Tipos de Variáveis/TiposVariaveis.cs
@@ -24,7 +24,7 @@ class Tipos_dados
 
      
 
-    //Vamos ebibir todas variaveis
+    //Vamos exibir todas variaveis
     Console.WriteLine(x);
     Console.WriteLine(a);
     Console.WriteLine(b);
