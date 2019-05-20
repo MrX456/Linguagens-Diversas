493e34518646e6a9165a4ea6f3ce6f0066859175
MrX456 <47821251+MrX456@users.noreply.github.com>
2019-05-20 12:36:28 -0300

    Add files via upload

---
+++ PHP/02-Variáveis e Constantes/Variaveis.php
@@ -0,0 +1,23 @@
+<?php
+
+//Em php não é preciso declarar os tipos, a linguagem identifica automaticamente
+//Variaveis em php são declaradas com $nomeVariavel
+
+    $x = 2;   //Inteiro
+    $y = 1.2; //Real
+    $nome = "Maria";  //String
+    $num = "21";  //String
+    $leitura = true;  //Booleano
+
+    //Exibindo variáveis
+    //Toda vez que variaveis forem chamadas devem conter $
+    echo $x;
+    echo'<br/>';
+    echo $y;
+    echo'<br/>';
+    echo $nome;
+    echo'<br/>';
+    echo $num;
+    echo'<br/>';
+    echo $leitura;//Em php booleanos são retornados como 1 = true e false não retorna nada
+?>
