9435bcfbc004e2a0838506375f3b5bed152c1390
MrX456 <47821251+MrX456@users.noreply.github.com>
2019-07-01 18:11:07 -0300

    Add files via upload

---
+++ C#/01-Olá Mundo/OlaMundo2.cs
@@ -0,0 +1,13 @@
+using System;
+
+    class OlaMundo2
+    {
+        static void Main(string[] args)
+        {
+            //Este comando mostra um texto na tela sem a quebra de linha
+            Console.Write("Olá");
+            Console.Write(" Mundo!");
+
+            Console.ReadLine();
+        }
+    }
