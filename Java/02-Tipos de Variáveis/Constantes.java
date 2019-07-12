cf72153ddf824cbf5e91011f1f14f79a55179fa8
MrX456 <47821251+MrX456@users.noreply.github.com>
2019-07-12 15:33:28 -0300

    Add files via upload

---
+++ Java/02-Tipos de Variáveis/Constantes.java
@@ -0,0 +1,14 @@
+class Constates {
+    public static void main(String[] args) {
+        // As constantes não podem ter seu valor alterado no decorrer do programa
+        // para declarar uma constante usamos a palavra final
+        final double pi = 3.14;
+        final String cidade = "Rio de Janeiro";
+        final int x = 3;
+
+        // exibindo
+        System.out.println(pi);
+        System.out.println(cidade);
+        System.out.println(x);
+    }
+}
