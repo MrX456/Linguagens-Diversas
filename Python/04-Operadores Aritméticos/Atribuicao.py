d725a541838ef1e3105be0ed6a7270f1cd84c277
MrX456 <47821251+MrX456@users.noreply.github.com>
2019-07-19 22:56:48 -0300

    Add files via upload

---
+++ Python/04-Operadores Aritméticos/Atribuicao.py
@@ -0,0 +1,28 @@
+#Não temos incremento o decremento em python mas temos atribuição
+
+a = 2
+b = 3
+c = 5
+d = 33
+e = 9
+
+#+= valor desejado a somar com a variavel(a = a + 1)
+a += 1
+
+#-= valor desejado a subtrair da variavel(b = b - 4)
+b -= 4
+
+#*= valor desejado a multiplicar com variavel(c = c * 3)
+c *= 3
+
+#*= valor desejado a dividir variavel(d = d / 3)
+d /= 3
+
+#%= atribução com resto da divisão(e = e % 5[resta 4])
+e %= 5
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
