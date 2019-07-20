0cdc7908aad77f69e9c3ae87abd16e6c6b47b298
MrX456 <47821251+MrX456@users.noreply.github.com>
2019-07-19 22:24:13 -0300

    Add files via upload

---
+++ Python/03-Entrada de Dados/Entrada.py
@@ -0,0 +1,10 @@
+#para receber dados do usuario usamos função input
+
+nome = input('Qual seu nome?')
+print(nome)
+
+idade = input('Qual sua idade?')
+print(idade)
+
+peso = input('Qual seu peso?')
+print(peso)
---
+++ Python/03-Entrada de Dados/Entrada2.py
@@ -0,0 +1,8 @@
+#todos dados vindo do input são do tipo str
+
+x = input('Digite um numero ')
+print(type(x))
+
+#Se quisermos outro tipo precisamos converter
+y = int(input('Digite outro numero '))
+print(type(y))
