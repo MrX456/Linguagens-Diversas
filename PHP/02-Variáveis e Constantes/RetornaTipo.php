0629a1dedc338f5f14b0c7e121e9afd2bc469d66
MrX456 <47821251+MrX456@users.noreply.github.com>
2019-07-29 01:00:50 -0300

    Add files via upload

---
+++ PHP/02-Variáveis e Constantes/RetornaTipo.php
@@ -0,0 +1,17 @@
+<?php
+    //Temos uma função que pode retornar qual o tipo da variavel
+    $a = 12;
+    $b = 267.346;
+    $c = "32";
+    $d = false;
+
+    //gettype retorna qual o tipo da variavel
+    echo gettype($a);
+    echo '<br/>';
+    echo gettype($b);
+    echo '<br/>';
+    echo gettype($c);
+    echo '<br/>';
+    echo gettype($d);
+    echo '<br/>';
+?>
